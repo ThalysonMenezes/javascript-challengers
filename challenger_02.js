eea0f7a6df25f84f4a30a6b24127ea1ca08b69ee
ThalysonMenezes <55069806+ThalysonMenezes@users.noreply.github.com>
2022-04-19 21:09:51 -0300

    commit do desafio 2

---
+++ challenger_02.js
@@ -0,0 +1,25 @@
+
+// Algoritmo que reverte strings
+// sem utilizar funções prontas do Javascript
+
+function FirstReverse(str) { 
+
+    // Adiciona cada elemento da string em uma lista
+    var str_list = [];
+    for (let l of str) str_list.push(l);
+
+    // Adiciona em uma nova lista os elementos na ordem inversa
+    var reverse_str = [];
+    for (let i = 1; i <= str_list.length; i++) reverse_str.push(str_list[str_list.length - i]);
+
+    // Concatena os elementos
+    var new_str = '';
+    for (let i = 0; i < reverse_str.length; i ++) new_str += reverse_str[i];
+
+    return new_str;
+
+  }
+     
+console.log(FirstReverse(" Oi Thalyson"));
+console.log();
+console.log(FirstReverse("Está funcionando!"));
